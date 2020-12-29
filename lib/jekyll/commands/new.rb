d18b7b65d2a2625d31af7e5f0261566a0345b9cb
PierrePh@rel <edo.tensei095@gmail.com>
2020-12-29 22:55:38 +0100

    bundler removed on new command

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -13,7 +13,6 @@ module Jekyll
 
             c.option "force", "--force", "Force creation even if PATH already exists"
             c.option "blank", "--blank", "Creates scaffolding but with empty files"
-            c.option "skip-bundle", "--skip-bundle", "Skip 'bundle install'"
 
             c.action do |args, options|
               Jekyll::Commands::New.process(args, options)
@@ -67,51 +66,12 @@ module Jekyll
 
         private
 
-        def gemfile_contents
-          <<~RUBY
-            source "https://rubygems.org"
-            # Hello! This is where you manage which Jekyll version is used to run.
-            # When you want to use a different version, change it below, save the
-            # file and run `bundle install`. Run Jekyll with `bundle exec`, like so:
-            #
-            #     bundle exec jekyll serve
-            #
-            # This will help ensure the proper Jekyll version is running.
-            # Happy Jekylling!
-            gem "jekyll", "~> #{Jekyll::VERSION}"
-            # This is the default theme for new Jekyll sites. You may change this to anything you like.
-            gem "minima", "~> 2.5"
-            # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
-            # uncomment the line below. To upgrade, run `bundle update github-pages`.
-            # gem "github-pages", group: :jekyll_plugins
-            # If you have any plugins, put them here!
-            group :jekyll_plugins do
-              gem "jekyll-feed", "~> 0.12"
-            end
-
-            # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
-            # and associated library.
-            platforms :mingw, :x64_mingw, :mswin, :jruby do
-              gem "tzinfo", "~> 1.2"
-              gem "tzinfo-data"
-            end
-
-            # Performance-booster for watching directories on Windows
-            gem "wdm", "~> 0.1.1", :platforms => [:mingw, :x64_mingw, :mswin]
-
-          RUBY
-        end
-
         def create_site(new_blog_path)
           create_sample_files new_blog_path
 
           File.open(File.expand_path(initialized_post_name, new_blog_path), "w") do |f|
             f.write(scaffold_post_content)
           end
-
-          #File.open(File.expand_path("Gemfile", new_blog_path), "w") do |f|
-          #  f.write(gemfile_contents)
-          #end
         end
 
         def preserve_source_location?(path, options)
@@ -137,31 +97,8 @@ module Jekyll
         # unless the user opts to generate a blank blog or skip 'bundle install'.
 
         def after_install(path, options = {})
-          unless options["blank"] || options["skip-bundle"]
-            begin
-              require "bundler"
-              bundle_install path
-            rescue LoadError
-              Jekyll.logger.info "Could not load Bundler. Bundle install skipped."
-            end
-          end
-
           Jekyll.logger.info "New jekyll site installed in #{path.cyan}."
           Jekyll.logger.info "Bundle install skipped." if options["skip-bundle"]
-        end
-
-        def bundle_install(path)
-          Jekyll.logger.info "Running bundle install in #{path.cyan}..."
-          Dir.chdir(path) do
-            exe = Gem.bin_path("bundler", "bundle")
-            process, output = Jekyll::Utils::Exec.run("ruby", exe, "install")
-
-            output.to_s.each_line do |line|
-              Jekyll.logger.info("Bundler:".green, line.strip) unless line.to_s.empty?
-            end
-
-            raise SystemExit unless process.success?
-          end
         end
       end
     end
